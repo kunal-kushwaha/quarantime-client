a6fda134c2dba24295ec87b08628a6d25531c1c3
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2020-06-05 03:03:45 +0530

    proxy

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://powerful-lake-29514.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
